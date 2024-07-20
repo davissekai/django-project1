f0559fc725ac254fd76838df67cc384c1016aee9
davissekai <davisdelali24@gmail.com>
2024-07-20 21:48:59 +0100

    more stuff.. sigh

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import render
 
 # Create your views here. My first view I might add :)
 
@@ -7,4 +9,23 @@ from django.http import HttpResponse
 def index(request):
    return HttpResponse("Greetings human, you have arrived at my polls site.")
 
-    
+
+def detail(request, question_id):
+   return HttpResponse("You're looking at question %s." % question_id  )
+
+
+def result(request, question_id):
+   response = "You're looking at the results of question %s."
+   return HttpResponse(response, question_id)
+
+def vote(request, question_id):
+   return HttpResponse("You're voting on question %s. % question_id")
+
+def index(request):
+   latest_question_list = Question.objects.order_by("pub_date")[:5]
+   
+   context = {
+      "latest_question_list": latest_question_list
+   }
+   return render(request, "polls/index.html", context)
+
